214a10b00b995529b368732c056d93cdfc45320f
witt1e <gowittie@gmail.com>
2025-03-11 15:26:20 +0900

    [refactor] while statement in main.play()

--- Baseball/main.swift
+++ Baseball/main.swift
@@ -12,20 +12,23 @@ play()
 func play() {
     var game = BaseballGame()
     var input: String?
+    var menu: Menu?
     
     // 플레이어가 Exit를 선택하기 전까지 게임 반복
-    while input != Menu.exit.rawValue {
+    while menu != .exit {
         showHomeScreen()
         
         input = readLine()
+        menu = Menu(rawValue: input!)
         
-        switch input {
-        case Menu.start.rawValue:
+        switch menu {
+        case .start:
             game.start()
-        case Menu.viewRecord.rawValue:
+        case .viewRecord:
             showRecord(records: game.gameRecords) // 질문: BaseballGame에서 gameRecords를 가져오는 시점은? 답: 함수 호출 시점
-        case Menu.exit.rawValue:
+        case .exit:
             print("See you next time!")
+            print("2025년 키움 히어로즈 우승을 기원합니다!!")
             game.gameRecords = [] // 게임 기록 초기화
         default:
             print("Invalid input. Please try again.")
